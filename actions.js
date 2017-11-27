c868b7916d4d27d132c20e5079a2ab643f3e3089
Dmitry Zherebko <kraken@live.ru>
2017-11-27 15:19:41 +0200

    initial

--- actions.js
+++
@@ -1,9 +0,0 @@
-const createAsyncActions = name => {
-  return {
-    success: createAction(name + "success"),
-    error: createAction(name + "error"),
-    busy: createAction(name + "busy")
-  };
-};
-
-const createAction = fn => {};
